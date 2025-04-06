d5c0bfddb8e772a924ad333dc0cc8762baf0c83d
victord1971 <biktopd@gmail.com>
2025-04-06 17:22:51 +0300

    T4

--- 932.py
+++ 932.py
@@ -1,7 +1,10 @@
 #import
+T4=0
 T16=5131
-for num in range(10_000,10_150):
+for num in range(11,200):
+#for num in range(10_000,10_150):
     snum=str(num)
     for digit in range(1,len(snum)):
         print(num, snum[0:digit], snum[digit:])
+    print()
 
