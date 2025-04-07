f06786be62e582c99993f6e1b878c0b424091708
victord1971 <biktopd@gmail.com>
2025-04-07 17:41:45 +0300

    full

--- 932.py
+++ 932.py
@@ -1,10 +1,16 @@
 #import
 T4=0
 T16=5131
-for num in range(11,200):
-#for num in range(10_000,10_150):
+for num in range(11,10_000_000_000_000_000):
+#for num in range(10_000,100_000):
     snum=str(num)
     for digit in range(1,len(snum)):
-        print(num, snum[0:digit], snum[digit:])
+        if snum[digit:digit+1] != '0':
+            te1=snum[0:digit]
+            te2=snum[digit:]
+            #print(num, snum[0:digit], snum[digit:])
+            print(num, te1, te2)
+            if (int(te1)+int(te2))**2 == num:
+                T4+=num
     print()
-
+print(T4)
