333325bf5af770226c31397c2ee78b171997e6a8
victord1971 <biktopd@gmail.com>
2025-04-10 09:52:41 +0300

    cccc

---
+++ 932.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    long T16, num;
+    printf("Hello, World!\n");
+    T16=0;
+    for (num=11; num<1000000; num++)
+    {
+        printf("%d\n", num);
+    }
+
+}
+
+
