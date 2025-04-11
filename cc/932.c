86b860a59b5f5b23c4983acce960c839e4aaf16c
victopd1971 <biktopd@gmail.com>
2025-04-11 18:56:28 +0300

    sprintf

--- cc/932.c
+++ cc/932.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 int main()
 {
+    char snum[18];
     long long T16, num;
     printf("Hello, World!\n");
     T16=0;
-    for (num=11; num<1000000; num++)
+    for (num=11; num<1000; num++)
     {
-        printf("%d\n", num);
+        sprintf(snum,"%d",num);
+        printf("%d  %s\n", num,snum);
     }
 
 }
