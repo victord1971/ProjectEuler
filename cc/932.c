dd8b17ec738cbc7f4b07f5bef5b27a34c9a96d36
victopd1971 <biktopd@gmail.com>
2025-04-11 19:14:07 +0300

    =\0

--- cc/932.c
+++ cc/932.c
@@ -3,13 +3,21 @@
 int main()
 {
     char snum[18];
+    int i;
     long long T16, num;
     printf("Hello, World!\n");
     T16=0;
     for (num=11; num<1000; num++)
     {
         sprintf(snum,"%d",num);
-        printf("%d  %s\n", num,snum);
+        printf("%d  %s  ", num,snum);
+        i=0;
+        while (snum[i]!='\0')
+        {
+            printf("%c",snum[i]);
+            i++;
+        }
+        printf("\n");
     }
 
 }
