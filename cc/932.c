5c97bf4c29fd65600f8ef81858f2cc7fa411a415
victopd1971 <biktopd@gmail.com>
2025-04-13 22:58:54 +0300

    check

--- cc/932.c
+++ cc/932.c
@@ -22,13 +22,16 @@ int main()
 {
     char snum[18], te1[17], te2[17];
     int i, len;
-    long long T16, T10, num, te1d, te2d, temp, check;
+    long long T16, T10, num, te1d, te2d, temp;
+    long long check;
     T16=0;
                     //1234567890123456
     for (num=11; num<10000000000000000; num++)
     {
-        check=sqrtl(num);
+        //check=sqrtl(num);
+        check=num;
         if(check*check!=num) {continue;}
+        
         sprintf(snum,"%lld",num);
         if(num%100000000==0)
             printf("    %ld\n", num/100000000);
