83a80a354f311023fe72860a05742d49aec33f74
victopd1971 <biktopd@gmail.com>
2025-04-18 22:08:13 +0300

    10

--- 37.py
+++ 37.py
@@ -1,5 +1,5 @@
 a=[2]
-for num in range(3,10_000):
+for num in range(3,100_000):
 	t=0
 	for x in a:
 		if num%x==0:
@@ -10,16 +10,27 @@ for num in range(3,10_000):
 	a.append(num)
 	#print(num)
 print(a)
+sum=0
+its=0
 for num in a:
 	print(num)
 	snum=str(num)
+	t=0
 	for digit in range(1,len(snum)):
-	#if snum[digit:digit+1] != '0':
 		if snum[digit:digit+1] != '0':
 			te1=snum[0:digit]
             #te1=snum[0:digit]
 			te2=snum[digit:]
 			#te2=snum[digit:]
-			# print('    ', te1, te2)
-	#print(' ')
+			print('    ', te1, te2)
+			if not(int(te1) in a):
+				t+=1; break
+			if not(int(te2) in a):
+				t+=1; break
+	if t==0 and num>22:
+		print('            ',num)
+		its+=1
+print(its)		
+
+
 
