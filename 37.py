694b493e51f92973636c23456c33d09fc847b4a9
victopd1971 <biktopd@gmail.com>
2025-04-19 17:47:23 +0300

    both2

--- 37.py
+++ 37.py
@@ -1,7 +1,7 @@
 a=[2]
 sum=0
 its=0
-for num in range(3,5_00_000):
+for num in range(3,5_000_000):
 	
 	t=0
 	for x in a:
@@ -19,17 +19,18 @@ for num in range(3,5_00_000):
 			te1=snum[0:digit]
             #te1=snum[0:digit]
 			te2=snum[digit:]
-			#te2=snum[digit:]
-			#print('    ', te1, te2)
 			if not(int(te1) in a):
 				t+=1; break
 			if not(int(te2) in a):
 				t+=1; break
 	if t==0 and num>22:
+		sum+=num
 		print('            ',num)
 		its+=1
-		print(its)		
-print(its)
+		print(its)	
+		if its==11:
+			break	
+print(sum)
 
 #for num in a:
 #	print(num)
