0c112fa6e4f570c02aa1f83f5f8e1c9ac26feb44
victord1971 <biktopd@gmail.com>
2025-05-15 21:05:58 +0300

    double2

--- 940/940.py
+++ 940/940.py
@@ -1,32 +1,22 @@
 #MOD = 1
 MOD = 1123581313
-K = 5
+k=50
+f=[0,1]
+for x in range(2,k+1):
+    f.append(f[x-1]+f[x-2])
+print(f)
+K = f[k]+1
+print(K)
+f.pop(0); f.pop(0)
+print(f)
 
 # Створюємо таблицю (K x K), заповнену нулями
-A = [[0 for _ in range(K)] for _ in range(K)]
+A = [0 for _ in range(K)]
 
-# Початкові умови
-A[0][0] = 0
+# Початкові умови A[0][0] = 0
 if K > 1:
-    A[0][1] = 1
-if K > 2:
-    A[0][2] = 1
-for n in range(3, K):
-    A[0][n] = (A[0][n-1] + A[0][n-2]) #% MOD
+    A[1] = 1
+print(A, A[0], A[1], A[2])
 
 # Заповнення таблиці згідно з рекурсіями
-for m in range(1, K):
-    for n in range(K):
-        if n == 0:
-            A[m][n] = (A[m-1][n] + A[m-1][n+1]) #% MOD
-        else:
-            A[m][n] = (2 * A[m][n-1] + A[m-1][n-1]) #% MOD
-
-# Обчислення суми S(K)
-result = 0
-for m in range(1, K):
-    for n in range(1, K):
-        result = (result + A[m][n]) 
-print(A)
-print(result % MOD)
-
+#for m in range(1, K):
