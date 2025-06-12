7d048f151b78951c696cb84b98db2dbddc0d9f18
victord1971 <biktopd@gmail.com>
2025-06-12 22:45:38 +0300

    better

---
+++ 940/940-7-2.py
@@ -0,0 +1,35 @@
+
+MOD = 1123581313
+k=5
+f=[0,1]; A=[]; An=[]
+for x in range(2,k+1):
+    f.append(f[x-1]+f[x-2])
+#print(f)
+K = f[k]+1
+print(K)
+f.pop(0); f.pop(0)
+#print(f)
+
+# Створюємо таблицю (K x K), заповнену нулями
+#Ao = [[0 for _ in range(K)] for _ in range(K)]
+
+# Початкові умови A[0][0] = 0
+if K > 1:
+    #Ao[0][1] = 1; A[1][0] = 1; A[1][1] = 2
+    A.append(1); A.append(2); A.append(1); median = 1
+    print(A)
+# Заповнення таблиці згідно з рекурсіями    
+'''
+for m in range(1, K):
+    for n in range(m+2):
+        if n < K:
+            if n == 0:  '''
+result = 2
+for m in range(2,K):
+    for i in range(0, median):
+        An.append(A[i]+A[i+1])
+    An.append(2*An[i]+A[i])
+    An.append(2*An[i+1]+A[i+1])
+    print(An)
+    A=An; An=[]; #median+=1
+    # Обчислення суми S(K)
