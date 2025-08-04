e51f7162cc458ca4a5f607c3c253c83809db49e6
victord1971 <biktopd@gmail.com>
2025-08-04 12:57:00 +0300

    08-03

--- 039/rust/src/main.rs
+++ 039/rust/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+
+struct U3 {
+    a: usize,
+    b: usize,
+    c: usize,
+    }
+fn main() {          
+    //const u3: [usize; 3];
+    let mut _piphagor =Vec::<U3>::new();
+    let _temp = U3 {a:1,b:2,c:3};
+    _piphagor.push(_temp);
+    println!("{}  {}  {}  ", _piphagor[0].a, _piphagor[0].b, _piphagor[0].c);
     println!("Hello, world!");
 }
---
+++ 039/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+d:\202x\2025\Python\ProjectEuler\039\rust\target\debug\rust.exe: d:\202x\2025\Python\ProjectEuler\039\rust\src\main.rs
