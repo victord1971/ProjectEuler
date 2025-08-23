b5ef4dc5f9272a7ff02560758cad64dd893169c5
victord1971 <biktopd@gmail.com>
2025-08-23 12:05:22 +0300

    08-22

--- 059/rust/src/main01.rs
+++ 059/rust/src/main01.rs
@@ -6,7 +6,6 @@ fn main() {
     let mut temp2;
     let mut _content =Vec::<u8>::new();
     let mut _char = String::new();
-    let mut _passw = String::new();
     let mut poch = 0;
     let mut kinec = 1;
     const WORDS: [&str; 8] = ["the","be","to","of","and","in","that","have"];
@@ -44,17 +43,4 @@ fn main() {
     println!("{}",_content.len());    
     println!("{:?}", _char);           
     println!("{}",_char.len());
-
-    for x in 97..123 {
-        for y in 97..123 {
-            for z in 97..123 {
-                _passw.push(x as u8 as char);
-                _passw.push(y as u8 as char);
-                _passw.push(z as u8 as char);
-                //println!("{:?}", _passw);
-
-                _passw.clear();
-            }
-        }
-    }
 }
